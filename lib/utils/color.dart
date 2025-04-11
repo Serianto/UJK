3152e72645d1b1969532da165c12c65a80848e38
Serianto <sendy.arief.febrianto02@gmail.com>
2025-04-11 14:38:00 +0700

    initial commit

---
+++ lib/utils/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color bg = Color(0xFFDDEB9D);
+const Color satu = Color(0xFFA0C878);
+const Color dua = Color(0xFF143D60);
+const Color tiga = Color(0xFFEB5B00);
+const Color empat = Color(0xFF5F8B4C);
+const Color lima = Color(0xFFFFDDAB);
+const Color enam = Color(0xFFFF9A9A);
+const Color tujuh = Color(0xFF945034);
